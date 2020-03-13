eddf306f977c085fc4946501874770afe09d19b1
Huy. Ly Gia <huylg@vng.com.vn>
2020-03-13 16:24:14 +0700

    find string

---
+++ FindString.py
@@ -0,0 +1,17 @@
+def count_substring(string, sub_string):
+
+    result = 0
+    for i in range(len(string)-len(sub_string)+1):
+        temp = 1
+        for j in range(len(sub_string)):
+            if(string[i+j] != sub_string[j]):
+                temp = 0
+        result += temp
+    return result
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
