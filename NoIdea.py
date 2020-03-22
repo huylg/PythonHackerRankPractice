1432245fad9b82a0a6efafe22a1fd2ca515874fa
huylg <huylg@vng.com.vn>
2020-03-22 13:40:16 +0700

    No idea complete

--- NoIdea.py
+++ NoIdea.py
@@ -1,14 +1,12 @@
+from collections import Counter
 n,m = map(int,input().split())
-arr = list(map(int,input().split()))
+arr = Counter(map(int,input().split()))
 a = set(map(int,input().split()))
 b = set(map(int,input().split()))
 result = 0
-for i in a:
-    if i in arr:
-        result += 1
-for i in b:
-    if i in arr:
-        result -= 1
+for i in a:result += arr[i]
+
+for i in b:result -= arr[i]
 
 
 print(result)
