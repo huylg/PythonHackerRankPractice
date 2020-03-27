a2762a7240638470753b1b82cd9bb24615f4b976
Huy. Ly Gia <huylg@vng.com.vn>
2020-03-27 18:30:02 +0700

    xml practice

--- XML/FindTheScore.py
+++ XML/FindTheScore.py
@@ -1,5 +1,18 @@
+import sys
 import xml.etree.ElementTree as etree
-xml =  "\n".join( [ input() for _ in range(int(input()))])
-tree = etree.ElementTree(etree.fromstring(xml))
-print(tree.getroot().attrib)
-print(list(tree.getroot()))
+
+def get_attr_number(node):
+    elements = [node]
+    result = 0
+    while elements:
+        temp = elements.pop(0)
+        elements += [i for i in list(temp)]
+        result += len(temp.attrib)
+    return result
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
